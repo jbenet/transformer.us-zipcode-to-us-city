2c28deff799d73b7055ee60005d11a6e8dadd669
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 04:02:38 -0700

    new version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var transformer = require('dat-transformer');
-var tUsZipcode = transformer('us-zipcode');
-var tUsCity = transformer('us-city');
+var Conversion = require('transformer-conversion');
+var tUsZipcode = require('transformer.us-zipcode');
+var tUsCity = require('transformer.us-city');
 // require any other modules you may need here.
 var zipdb = require('zippity-do-dah');
 
-module.exports = transformer.Conversion(tUsZipcode, tUsCity, convert);
+module.exports = Conversion(tUsZipcode, tUsCity, convert);
 
 function convert(zipcode) {
   var r = zipdb.zipcode(zipcode);
